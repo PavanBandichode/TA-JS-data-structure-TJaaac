10aa2ad18bebabf30d8d3a21236f2ad9ecc0a854
PavanBandichode <pavan.bandichode@gmail.com>
2021-11-15 16:53:01 +0530

    assignment level 2:assignment 2

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,19 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-
+let collection =[numbers ,userIds];
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
-
+for(let evenCollection of collection){
+  if(evenCollection%2==0){
+    console.log(evenCollection);
+  }
+}
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
-
+for(let evenCollection of collection){
+  if(evenCollection%2==0){
+    console.log(evenCollection);
+  }
+}
 /*
   @param means parameter
 
